c45dee16b0334ad69168ec3b471f7f8f4a8f24b5
Oscar702 <FunesLV@aol.com>
2021-01-11 21:56:15 +0000

    one

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
-//create your first component
-export function Home() {
+export function Home(props) {
+	const [tasks, setTasks] = useState([
+		"running",
+		"eating",
+		"walking",
+		"sleeping",
+		"studying"
+	]);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input
+				type="text"
+				id="fname"
+				name="fname"
+				placeholder="first job"
+			/>
+			<ul className="list-group">
+				{tasks.map((task, i) => {
+					return <li key={i}> {task}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
